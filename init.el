1e3e6c0c2a9544e67be21e75944031db7f21902b
aila <aila@escherlat.com>
2025-05-11 21:49:32 -0700

    Remove older themeing and line numbering support

--- init.el
+++ init.el
@@ -1,4 +1,5 @@
 (setq load-path (cons "~/.emacs.d/lisp" load-path))
+(global-display-line-numbers-mode)
 
 (load "nxml-mode/rng-auto.el")
 
@@ -17,16 +18,7 @@
 
 (setq org-agenda-files (list "~/Dropbox/cPanel/Notes"))
 
-;; Better line numbering
-(setq linum-format "%d ")
-
-;; Colors :)
-(require 'color-theme)
-(color-theme-initialize)
-(color-theme-comidia)
-
-(require 'setnu)
-(setnu-mode t)
+(load-theme 'dracula t)
 
 ;; Show column number in mode line
 (column-number-mode)
@@ -37,12 +29,19 @@
 (setq-default indent-tabs-mode nil)
 ;; Use 4 space indents via cperl mode
 (custom-set-variables
-  '(cperl-close-paren-offset -4)
-  '(cperl-continued-statement-offset 4)
-  '(cperl-indent-level 4)
-  '(cperl-indent-parens-as-block t)
-  '(cperl-tab-always-indent t)
-)
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cperl-close-paren-offset -4)
+ '(cperl-continued-statement-offset 4)
+ '(cperl-indent-level 4)
+ '(cperl-indent-parens-as-block t)
+ '(cperl-tab-always-indent t)
+ '(custom-safe-themes
+   '("603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961"
+     default))
+ '(package-selected-packages '(dracula-theme)))
 
 ;;http://gist.github.com/128694
 ;; perltidy region with <strg><c> <t>
@@ -105,3 +104,9 @@
 
 (eval-after-load "vc-git" '(fset 'vc-git-annotate-command 'pek-vc-git-annotate-command))
 (eval-after-load "vc-git" '(fset 'vc-git-annotate-time 'pek-vc-git-annotate-time))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
